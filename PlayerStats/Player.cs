ab50a4ef3505938e5d6edfbdc9a36b9434686007
matv <matv.oficial@gmail.com>
2024-04-12 23:01:43 +0100

    PlayerStats - Score  & WinRate Properties

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -7,6 +7,33 @@ namespace PlayerStats
         private int wonGames;
         public string Name { get; }
 
+        public float HighScore
+        {
+            get
+            {
+                return highScore;
+            }
+            set
+            {
+                if (value > highScore) highScore = value;
+            }
+        }
+
+        public float WinRate
+        {
+            get
+            {
+                if (playedGames == 0)
+                {
+                    return 0.0f;
+                }
+                else
+                {
+                    return (float)wonGames / playedGames;
+                }
+            }
+        }
+
         public Player(string name)
         {
             Name = name;
